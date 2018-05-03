3da06af1fc3e90ed906ed13aa0bf9129edea845f
Bryan_lzh <390807154@qq.com>
2018-05-03 16:04:43 +0800

    JDK8 BUG

--- src/Br/API/Scripts/ScriptLoader.java
+++ src/Br/API/Scripts/ScriptLoader.java
@@ -62,6 +62,26 @@ public class ScriptLoader {
     }
 
     public static NashornScriptEngine eval(Plugin p, Consumer<NashornScriptEngine> c) {
+        /*  
+        nashorn脚本引擎存在一个明显的BUG
+        位于 jdk.nashorn.api.scripting.NashornScriptEngineFactory:431的静态方法getAppClassLoader
+        其中字节码描述如下: 
+             * 0: invokestatic  java/lang/Thread.currentThread:()Ljava/lang/Thread;
+             * 3: invokevirtual java/lang/Thread.getContextClassLoader:()Ljava/lang/ClassLoader;
+             * 6: astore_0
+             * 7: aload_0
+             * 8: ifnonnull     19
+             * 11: ldc           jdk/nashorn/api/scripting/NashornScriptEngineFactory
+             * 13: invokevirtual java/lang/Class.getClassLoader:()Ljava/lang/ClassLoader;
+             * 16: goto          20
+             * 19: aload_0
+             * 20: areturn
+        显然 代码返回的类加载器首先选取当前线程的类加载器(位于Thread.currentThread().getContextClassLoader())
+        若返回null则返回NashornScriptEngineFactory的类加载器(NashornScriptEngineFactory.class.getClassLoader())
+        这意味着 通过构造ScriptEngineManager所传入的类加载器没有任何用处
+        导致返回的ScriptEngine不使用所指定的类加载器
+        故本方法做了修改当前线程类加载器的修改
+        */
         ClassLoader backup = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader(p.getClass().getClassLoader());
         ScriptEngineManager EngineManager = new ScriptEngineManager(p.getClass().getClassLoader());
